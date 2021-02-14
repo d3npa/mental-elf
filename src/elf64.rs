93268cac6702310548dc50eff6265a952fb779a6
d3npa <d3npa@pm.me>
2021-02-14 13:29:31 -0500

    Refactor struct names to reduce redundancy and add convenience function to save program headers

--- src/elf64.rs
+++ src/elf64.rs
@@ -1,10 +1,10 @@
 use std::mem;
 
-pub const ELF64_HEADER_SIZE: usize = 0x40;
+pub const ELF_HEADER_SIZE: usize = 0x40;
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct Elf64Header {
+pub struct ElfHeader {
     pub e_ident: [u8; 16],
     pub e_type: u16,
     pub e_machine: u16,
@@ -21,25 +21,25 @@ pub struct Elf64Header {
     pub e_shstrndx: u16,
 }
 
-impl Elf64Header {
-    pub fn from_bytes(buffer: [u8; ELF64_HEADER_SIZE]) -> Elf64Header {
+impl ElfHeader {
+    pub fn from_bytes(buffer: [u8; ELF_HEADER_SIZE]) -> ElfHeader {
         unsafe {
-            mem::transmute::<[u8; ELF64_HEADER_SIZE], Elf64Header>(buffer)
+            mem::transmute::<[u8; ELF_HEADER_SIZE], ElfHeader>(buffer)
         }
     }
 
-    pub fn to_bytes(self) -> [u8; ELF64_HEADER_SIZE] {
+    pub fn to_bytes(self) -> [u8; ELF_HEADER_SIZE] {
         unsafe {
-            mem::transmute::<Elf64Header, [u8; ELF64_HEADER_SIZE]>(self)
+            mem::transmute::<ElfHeader, [u8; ELF_HEADER_SIZE]>(self)
         }
     }
 }
 
-pub const ELF64_PHDR_SIZE: usize = 0x38;
+pub const PROGRAM_HEADER_SIZE: usize = 0x38;
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct Elf64ProgramHeader {
+pub struct ProgramHeader {
     pub p_type: u32,
     pub p_flags: u32,
     pub p_offset: u64,
@@ -50,16 +50,16 @@ pub struct Elf64ProgramHeader {
     pub p_align: u64,
 }
 
-impl Elf64ProgramHeader {
-    pub fn from_bytes(buffer: [u8; ELF64_PHDR_SIZE]) -> Elf64ProgramHeader {
+impl ProgramHeader {
+    pub fn from_bytes(buffer: [u8; PROGRAM_HEADER_SIZE]) -> ProgramHeader {
         unsafe {
-            mem::transmute::<[u8; ELF64_PHDR_SIZE], Elf64ProgramHeader>(buffer)
+            mem::transmute::<[u8; PROGRAM_HEADER_SIZE], ProgramHeader>(buffer)
         }
     }
 
-    pub fn to_bytes(self) -> [u8; ELF64_PHDR_SIZE] {
+    pub fn to_bytes(self) -> [u8; PROGRAM_HEADER_SIZE] {
         unsafe {
-            mem::transmute::<Elf64ProgramHeader, [u8; ELF64_PHDR_SIZE]>(self)
+            mem::transmute::<ProgramHeader, [u8; PROGRAM_HEADER_SIZE]>(self)
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
 pub mod utils;
 pub mod elf64;
 
-use utils::Result;
-use elf64::Elf64Header;
-use elf64::Elf64ProgramHeader;
+use utils::{Result, StringError};
+use elf64::ElfHeader;
+use elf64::ProgramHeader;
 
 use std::io::prelude::*;
 use std::io::SeekFrom;
 use std::fs::File;
 
-pub fn read_elf64_header(fd: &mut File) -> Result<Elf64Header> {
-    let mut buffer = [0u8; elf64::ELF64_HEADER_SIZE];
+pub fn read_elf64_header(fd: &mut File) -> Result<ElfHeader> {
+    let mut buffer = [0u8; elf64::ELF_HEADER_SIZE];
     fd.seek(SeekFrom::Start(0))?;
     fd.read(&mut buffer)?;
-    Ok(Elf64Header::from_bytes(buffer))
+    Ok(ElfHeader::from_bytes(buffer))
 }
 
-pub fn write_elf64_header(fd: &mut File, header: Elf64Header) -> Result<()> {
+pub fn write_elf64_header(fd: &mut File, header: ElfHeader) -> Result<()> {
     let buffer = header.to_bytes();
     fd.seek(SeekFrom::Start(0))?;
     fd.write(&buffer)?;
@@ -25,17 +25,37 @@ pub fn write_elf64_header(fd: &mut File, header: Elf64Header) -> Result<()> {
 
 pub fn read_elf64_program_headers(
     fd: &mut File, phdr_offset: u64, phdr_num: u16) 
-    -> Result<Vec<Elf64ProgramHeader>> 
+    -> Result<Vec<ProgramHeader>> 
 {
     fd.seek(SeekFrom::Start(phdr_offset))?;
     
     let mut program_headers = Vec::new();
 
     for _ in 0..phdr_num {
-        let mut buffer = [0u8; elf64::ELF64_PHDR_SIZE];
+        let mut buffer = [0u8; elf64::PROGRAM_HEADER_SIZE];
         fd.read(&mut buffer)?;
-        program_headers.push(Elf64ProgramHeader::from_bytes(buffer));
+        program_headers.push(ProgramHeader::from_bytes(buffer));
     }
 
     Ok(program_headers)
+}
+
+pub fn write_elf64_program_headers(
+    fd: &mut File, phdr_offset: u64, phdr_num: u16, 
+    program_headers: Vec<ProgramHeader>) -> Result<()> 
+{
+    if phdr_num as usize != program_headers.len() {
+        return Err(StringError::boxed(
+            &format!("Attempting to write more or fewer program headers than the file contains: {}:{}", program_headers.len(), phdr_num)
+        ));
+    }
+
+    fd.seek(SeekFrom::Start(phdr_offset))?;
+
+    for header in program_headers {
+        let buffer = header.to_bytes();
+        fd.write(&buffer)?;
+    }
+
+    Ok(())
 }
